970e78f3b36951879f08b88744aecd3211df4b0f
ashutoshtiwari367 <ashutoshtiwari9453@gmail.com>
2026-05-01 11:41:29 +0530

    Add fallback email address to prevent invalid from address error

--- contact.php
+++ contact.php
@@ -9,12 +9,13 @@ if (file_exists(__DIR__ . '/includes/mail-config.php')) {
 }
 
 if (!defined('MAIL_HOST'))      define('MAIL_HOST', 'smtp.hostinger.com');
-if (!defined('MAIL_USERNAME'))  define('MAIL_USERNAME', '');
+if (!defined('MAIL_USERNAME'))  define('MAIL_USERNAME', 'info@logixcode.com');
 if (!defined('MAIL_PASSWORD'))  define('MAIL_PASSWORD', '');
 if (!defined('MAIL_PORT'))      define('MAIL_PORT', 465);
-if (!defined('MAIL_FROM'))      define('MAIL_FROM', '');
+if (!defined('MAIL_FROM'))      define('MAIL_FROM', 'info@logixcode.com');
 if (!defined('MAIL_FROM_NAME')) define('MAIL_FROM_NAME', 'LogixCode');
-if (!defined('MAIL_TO'))        define('MAIL_TO', '');
+if (!defined('MAIL_TO'))        define('MAIL_TO', 'info@logixcode.com');
+
 
 
 
